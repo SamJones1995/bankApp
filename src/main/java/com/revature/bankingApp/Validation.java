5abe3b9a133a6a06db36ddb44d3d5800d47c50c1
SamJones1995 <88803371+SamJones1995@users.noreply.github.com>
2022-08-18 21:23:10 -0400

    Initial Commit

---
+++ src/main/java/com/revature/bankingApp/Validation.java
@@ -0,0 +1,20 @@
+package com.revature.bankingApp;
+
+public class Validation {
+
+	public void validate(String passwordUsr) {
+		if (passwordUsr.equals("LOGIN")) {
+			System.out.println("you're ready to log in");
+			
+			// LOGIN instructions
+		}
+		if (passwordUsr.equals("REGISTER")){
+			System.out.println("Please enter Username do not exceed 12 characters");
+		
+			
+		}
+//			
+			}
+//		
+	}
+	
